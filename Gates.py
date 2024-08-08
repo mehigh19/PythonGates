85a2e0a76cf8b1ce4a784e164833f73466074e26
unknown <mihaitg19@gmail.com>
2024-08-08 13:19:21 +0300

    Added abstract class Gates

---
+++ Gates.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Gates(ABC):
+
+    @abstractmethod
+    def readFile(self):
+        pass
+
+    @abstractmethod
+    def saveFile(self):
+        pass
+
+    @abstractmethod
+    def postToDB(self):
+        pass
