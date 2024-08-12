53f86b7015b262c767adc763765320b20529cd25
unknown <mihaitg19@gmail.com>
2024-08-12 05:08:53 +0300

    Bugfixes

--- Gate2.py
+++ Gate2.py
@@ -1,11 +1,18 @@
-
+from flask import jsonify
+import json
 
 class Gate2():
     def readFile(self):
-        readPath = r'HW\Poarta1.txt'
+        readPath = r'HW\Poarta2.csv'
         with open(readPath, 'r') as readFile:
             readContent = readFile.read()
             return readContent
 
     def saveFile(self):
-        pass
+        file_content = self.readFile()
+        lines = file_content.strip().splitlines()
+        json_content = {"file_content": lines}
+        save_path = r'HW\PythonGates\Poarta2.json'
+        with open(save_path, 'w') as json_file:
+            json.dump(json_content, json_file, indent=4)
+        return jsonify(json_content)
