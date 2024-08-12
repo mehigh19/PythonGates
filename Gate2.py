190bdab501eae099a11d7279d69d57d03da32655
unknown <mihaitg19@gmail.com>
2024-08-12 05:16:36 +0300

    Bugfixes

--- Gate2.py
+++ Gate2.py
@@ -1,18 +1,11 @@
-from flask import jsonify
-import json
+
 
 class Gate2():
     def readFile(self):
-        readPath = r'HW\Poarta2.csv'
+        readPath = r'HW\Poarta1.txt'
         with open(readPath, 'r') as readFile:
             readContent = readFile.read()
             return readContent
 
     def saveFile(self):
-        file_content = self.readFile()
-        lines = file_content.strip().splitlines()
-        json_content = {"file_content": lines}
-        save_path = r'HW\PythonGates\Poarta2.json'
-        with open(save_path, 'w') as json_file:
-            json.dump(json_content, json_file, indent=4)
-        return jsonify(json_content)
+        pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Gate1 import Gate1
 from Gate2 import Gate2
 from AddToDB import AddToDB
 from flask import Flask,request, jsonify
-import os
+import json
 
 poarta1=Gate1()
 poarta1.saveFile()
@@ -10,9 +10,15 @@ poarta1.saveFile()
 app=Flask(__name__)
 
 @app.route('/test')
-def addFile():
-    poarta2=Gate2()
-    return poarta2.saveFile()
+def addJson():
+    poarta2 = Gate2()
+    file_content = poarta2.readFile()
+    lines = file_content.strip().splitlines()
+    json_content = {"file_content": lines}
+    save_path = r'HW\PythonGates\Poarta2.json'
+    with open(save_path, 'w') as json_file:
+        json.dump(json_content, json_file, indent=4)
+    return jsonify(json_content)
 
 @app.route('/test',methods=['POST'])
 def postToDB():
